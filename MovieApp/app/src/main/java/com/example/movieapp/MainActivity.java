77f69459a95482758c5b074c4ef177ceec16ef9c
Robin <robin.becx@hotmail.com>
2020-03-25 16:58:51 +0100

    Demo version (Local files)

--- MovieApp/app/src/main/java/com/example/movieapp/MainActivity.java
+++ MovieApp/app/src/main/java/com/example/movieapp/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.movieapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import com.example.movieapp.AppLogic.RecyclerViewAdapter;
 import com.example.movieapp.DataStorrage.AsyncResponse;
 import com.example.movieapp.Domain.MovieElements;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements AsyncResponse {
     public ImageView imageView2;
@@ -17,6 +23,7 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse {
     public Button date;
     public Button rating;
     public Button expected;
+    List<Movie> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,6 +77,34 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse {
                 expected.setTextColor(Color.parseColor("#ffa500"));
             }
         });
+        movies = new ArrayList<>();
+        movies.add(new Movie("Battlefield Earth","2020",R.drawable.cover1));
+        movies.add(new Movie("john wick","2003",R.drawable.cover2));
+        movies.add(new Movie("john wick 2","2009",R.drawable.cover1));
+        movies.add(new Movie("iron man","2012",R.drawable.cover3));
+        movies.add(new Movie("thor","1980",R.drawable.cover1));
+        movies.add(new Movie("lion king","2004",R.drawable.cover2));
+        movies.add(new Movie("harry potter","201",R.drawable.cover2));
+        movies.add(new Movie("Battlefield Earth","2020",R.drawable.cover1));
+        movies.add(new Movie("john wick","2003",R.drawable.cover2));
+        movies.add(new Movie("john wick 2","2009",R.drawable.cover1));
+        movies.add(new Movie("iron man","2012",R.drawable.cover3));
+        movies.add(new Movie("thor","1980",R.drawable.cover1));
+        movies.add(new Movie("lion king","2004",R.drawable.cover2));
+        movies.add(new Movie("harry potter","201",R.drawable.cover2));
+        movies.add(new Movie("Battlefield Earth","2020",R.drawable.cover1));
+        movies.add(new Movie("john wick","2003",R.drawable.cover2));
+        movies.add(new Movie("john wick 2","2009",R.drawable.cover1));
+        movies.add(new Movie("iron man","2012",R.drawable.cover3));
+        movies.add(new Movie("thor","1980",R.drawable.cover1));
+        movies.add(new Movie("lion king","2004",R.drawable.cover2));
+        movies.add(new Movie("harry potter","201",R.drawable.cover2));
+
+        RecyclerView myrv = (RecyclerView) findViewById(R.id.recycleview);
+        RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(this,movies);
+        myrv.setLayoutManager(new GridLayoutManager(this,3));
+        myrv.setAdapter(myAdapter);
+
     }
 
     @Override
