80989f8a21909c8fd97d868a0215c62bd4f1ff19
Rik van Laerhoven <rikvanlaerhoven@hotmail.com>
2020-03-27 15:26:33 +0100

    Picture url builders toegevoegd
    Methodes gemaakt om de url voor een plaatje te bouwen, één methode voor een w200 formaat en één methode voor een w500 formaat.

--- MovieApp/app/src/main/java/com/example/movieapp/DataStorrage/NetworkUtils.java
+++ MovieApp/app/src/main/java/com/example/movieapp/DataStorrage/NetworkUtils.java
@@ -11,6 +11,9 @@ import java.util.Scanner;
 
 public class NetworkUtils {
     private static final String mString_url = "https://api.themoviedb.org/3/movie/popular?api_key=018bccaff77c7e87b7a1ba9af79aed2c&language=en-US&page=1";
+    private static final String mString_pictureUrlW500 = "https://image.tmdb.org/t/p/w500";
+    private static final String mString_pictureUrlW200 = "https://image.tmdb.org/t/p/w200";
+
 
     public static String getResponseFromHttpUrl(URL url) throws IOException {
 
@@ -29,20 +32,47 @@ public class NetworkUtils {
             } else {
                 return null;
             }
-        }finally {
+        } finally {
             urlConnection.disconnect();
         }
     }
 
-    public static URL buildUrl(){
+    public static URL buildUrl() {
         Log.d("NetworkUtils", "buildUrl: ");
         URL mUrl = null;
         try {
             mUrl = new URL(mString_url);
-        }catch (MalformedURLException e){
+        } catch (MalformedURLException e) {
             e.printStackTrace();
         }
         Log.d("NetworkUrl", "buildUrl: " + mUrl);
         return mUrl;
     }
+
+    public static URL buildImageUrlW500(String inputPath) {
+        Log.d("NetworkUtils", "buildImageUrlW500: ");
+        URL mUrl = null;
+        try {
+            mUrl = new URL(mString_pictureUrlW500 + inputPath);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        Log.d("NetworkUrl", "buildImageUrlW500: " + mUrl);
+        return mUrl;
+    }
+
+    public static URL buildImageUrlW200(String inputPath) {
+        Log.d("NetworkUtils", "buildImageUrlW200: ");
+        URL mUrl = null;
+        try {
+            mUrl = new URL(mString_pictureUrlW200 + inputPath);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        Log.d("NetworkUrl", "buildImageUrlW200: " + mUrl);
+        return mUrl;
+    }
 }
+
+
+
