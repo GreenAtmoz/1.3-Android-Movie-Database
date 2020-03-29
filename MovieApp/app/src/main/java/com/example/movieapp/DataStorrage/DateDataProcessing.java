0ae9c82a7191a48f5310d7f52f87aeba47bdd318
Ruben van de Rande <ruben.vanderande@gmail.com>
2020-03-29 20:38:38 +0200

    V1 Button Functions

--- MovieApp/app/src/main/java/com/example/movieapp/DataStorrage/DateDataProcessing.java
+++ MovieApp/app/src/main/java/com/example/movieapp/DataStorrage/DateDataProcessing.java
@@ -2,20 +2,22 @@ package com.example.movieapp.DataStorrage;
 
 import android.os.AsyncTask;
 import android.util.Log;
+
 import com.example.movieapp.Domain.MovieElements;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.net.URL;
 import java.util.ArrayList;
+
 import static android.content.ContentValues.TAG;
 
-public class PopularDataProcessing extends AsyncTask<String, Void, ArrayList<MovieElements>> {
+public class DateDataProcessing extends AsyncTask<String, Void, ArrayList<MovieElements>> {
     public AsyncResponse asyncResponse = null;
     private ArrayList<MovieElements> movieElements;
     private static final int aantalPaginas = 5; //het aantal pagina's met resultaten die worden weergegeven. Iedere pagina bevat 20 resultaten.
 
-    public PopularDataProcessing(ArrayList<MovieElements> movieElements) {
+    public DateDataProcessing(ArrayList<MovieElements> movieElements) {
         this.movieElements = movieElements;
     }
 
@@ -26,7 +28,7 @@ public class PopularDataProcessing extends AsyncTask<String, Void, ArrayList<Mov
             int page = 0;
             for (int j = 0; j < aantalPaginas; j++) {
                 page++;
-                String jsonResponseString = NetworkUtils.getResponseFromHttpUrl(NetworkUtils.buildPopularUrl(page));
+                String jsonResponseString = NetworkUtils.getResponseFromHttpUrl(NetworkUtils.buildDateUrl(page));
                 JSONObject object = new JSONObject(jsonResponseString);
                 JSONArray results = object.getJSONArray("results");
                 for (int i = 0; i < results.length(); i++) {
