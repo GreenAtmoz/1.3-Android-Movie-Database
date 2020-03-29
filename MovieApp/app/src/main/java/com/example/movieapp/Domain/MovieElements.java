186c1a44283a793c5cda8a8aa7280e3bfbde0fa0
Rik van Laerhoven <rikvanlaerhoven@hotmail.com>
2020-03-29 20:39:11 +0200

    rating int naar double

--- MovieApp/app/src/main/java/com/example/movieapp/Domain/MovieElements.java
+++ MovieApp/app/src/main/java/com/example/movieapp/Domain/MovieElements.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class MovieElements implements Serializable {
     private String filmTitel;
     private String description;
-    private int rating;
+    private double rating;
     private String trailerLink;
     private String imageUrlW200;
     private String imageUrlW500;
@@ -39,7 +39,7 @@ public class MovieElements implements Serializable {
         this.description = description;
     }
 
-    public void setRating(int rating) {
+    public void setRating(double rating) {
         this.rating = rating;
     }
 
@@ -55,7 +55,7 @@ public class MovieElements implements Serializable {
         return description;
     }
 
-    public int getRating() {
+    public double getRating() {
         return rating;
     }
 
