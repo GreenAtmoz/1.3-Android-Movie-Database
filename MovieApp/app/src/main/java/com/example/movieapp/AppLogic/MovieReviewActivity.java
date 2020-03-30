dfc9091a620929686b33257f25335d7bdb93a685
Ruben van de Rande <ruben.vanderande@gmail.com>
2020-03-30 17:11:04 +0200

    Intents voor to ReviewActivity

--- MovieApp/app/src/main/java/com/example/movieapp/AppLogic/MovieReviewActivity.java
+++ MovieApp/app/src/main/java/com/example/movieapp/AppLogic/MovieReviewActivity.java
@@ -6,16 +6,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.movieapp.Domain.MovieElements;
 import com.example.movieapp.Domain.ReviewElements;
 import com.example.movieapp.R;
 
 import java.util.ArrayList;
 
-public class MovieReview extends AppCompatActivity {
+public class MovieReviewActivity extends AppCompatActivity {
     private RecyclerView reviewRecylerview;
     private RecyclerView.LayoutManager reviewLayoutManager;
     private RecyclerView.Adapter reviewAdapter;
     private ArrayList<String> reviewElements;
+    private MovieElements movieElement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +30,9 @@ public class MovieReview extends AppCompatActivity {
         reviewElements.add("nooit");
         reviewElements.add("meer");
         reviewElements.add("werken");
+
+        Bundle extras = getIntent().getExtras();
+        movieElement = (MovieElements) extras.getSerializable("ELEMENT");
 
         reviewRecylerview = (RecyclerView) findViewById(R.id.reviewrecyclerview);
         reviewLayoutManager = new LinearLayoutManager(this);
