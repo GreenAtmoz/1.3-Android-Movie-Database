b305dae301719dc6bab6e4784b4b4c5edc3a2c43
Ruben van de Rande <ruben.vanderande@gmail.com>
2020-03-30 16:44:48 +0200

    More Tests

--- MovieApp/app/src/test/java/com/example/movieapp/DataStorrage/NetworkUtilsTest.java
+++ MovieApp/app/src/test/java/com/example/movieapp/DataStorrage/NetworkUtilsTest.java
@@ -9,12 +9,10 @@ import java.net.URL;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NetworkUtilsTest {
-
+    //Popular
     @Test
-    void testBuildPopularUrl() throws Exception {
-        final URL baseURLDutch = new URL("https://api.themoviedb.org/3/movie/popular?api_key=018bccaff77c7e87b7a1ba9af79aed2c&language=nl&page=1");
+    void testBuildPopularUrlEnglish() throws Exception {
         final URL baseURLEnglish = new URL("https://api.themoviedb.org/3/movie/popular?api_key=018bccaff77c7e87b7a1ba9af79aed2c&language=en-US&page=1");
-
         int page = 1;
         boolean result = false;
         NetworkUtils.language = Language.ENGLISH;
@@ -22,6 +20,90 @@ class NetworkUtilsTest {
         URL apiBuild = NetworkUtils.buildPopularUrl(page);
 
         if ((baseURLEnglish).equals(apiBuild)) {
+            result = true;
+        }
+
+        assertTrue(result);
+    }
+
+    @Test
+    void testBuildPopularUrlDutch() throws Exception {
+        final URL baseURLDutch = new URL("https://api.themoviedb.org/3/movie/popular?api_key=018bccaff77c7e87b7a1ba9af79aed2c&language=nl&page=1");
+        int page = 1;
+        boolean result = false;
+        NetworkUtils.language = Language.NEDERLANDS;
+
+        URL apiBuild = NetworkUtils.buildPopularUrl(page);
+
+        if ((baseURLDutch).equals(apiBuild)) {
+            result = true;
+        }
+
+        assertTrue(result);
+    }
+
+    //Date
+    @Test
+    void testBuildDateUrlEnglish() throws Exception {
+        final URL baseURLEnglish = new URL("https://api.themoviedb.org/3/discover/movie?api_key=018bccaff77c7e87b7a1ba9af79aed2c&language=en-US&sort_by=primary_release_date.dsc&page=1");
+        int page = 1;
+        boolean result = false;
+        NetworkUtils.language = Language.ENGLISH;
+
+        URL apiBuild = NetworkUtils.buildDateUrl(page);
+
+        if ((baseURLEnglish).equals(apiBuild)) {
+            result = true;
+        }
+
+        assertTrue(result);
+    }
+
+    @Test
+    void testBuildDateUrlDutch() throws Exception {
+        final URL baseURLDutch = new URL("https://api.themoviedb.org/3/discover/movie?api_key=018bccaff77c7e87b7a1ba9af79aed2c&language=nl&sort_by=primary_release_date.dsc&page=1");
+        int page = 1;
+        boolean result = false;
+        NetworkUtils.language = Language.NEDERLANDS;
+
+        URL apiBuild = NetworkUtils.buildDateUrl(page);
+
+        if ((baseURLDutch).equals(apiBuild)) {
+            result = true;
+        }
+
+        assertTrue(result);
+    }
+
+    //Rating
+    @Test
+    void testBuildRatingUrlEnglish() throws Exception {
+        final URL baseURLEnglish = new URL("https://api.themoviedb.org/3/movie/top_rated?api_key=018bccaff77c7e87b7a1ba9af79aed2c&language=en-US&page=1");
+        int page = 1;
+        boolean result = false;
+        NetworkUtils.language = Language.ENGLISH;
+
+        URL apiBuild = NetworkUtils.buildRatingUrl(page);
+
+        if ((baseURLEnglish).equals(apiBuild)) {
+            result = true;
+        }
+
+        System.out.println(apiBuild);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void testBuildRatingUrlDutch() throws Exception {
+        final URL baseURLDutch = new URL("https://api.themoviedb.org/3/movie/top_rated?api_key=018bccaff77c7e87b7a1ba9af79aed2c&language=en-US&page=1");
+        int page = 1;
+        boolean result = false;
+        NetworkUtils.language = Language.NEDERLANDS;
+
+        URL apiBuild = NetworkUtils.buildRatingUrl(page);
+
+        if ((baseURLDutch).equals(apiBuild)) {
             result = true;
         }
 
